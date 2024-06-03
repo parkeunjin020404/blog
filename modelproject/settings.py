8be290ff0ab554c75e19d1018393625048bab852
[parkeunjin020404] <[eunjin020404@naver.com]>
2024-06-03 21:45:34 +0900

    aSSA

--- modelproject/settings.py
+++ modelproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['i-05388419a58f809ec','eunjindev.site','127.0.0.1']
+ALLOWED_HOSTS = ['54.206.213.133','eunjindev.site','127.0.0.1']
 
 
 # Application definition
